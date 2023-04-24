a2021ad4fa06886fc1145978b5d738174cd9f369
durimfe <durimfetahaj@gmail.com>
2023-04-24 16:27:13 +0200

    added footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -2,13 +2,37 @@
 
 import styled from "@emotion/styled";
 import React from "react";
+import SocialMedia from "./SocialMedia";
 
-type Props = {};
-
-export default function index({}: Props) {
-  return <Wrapper>Footer</Wrapper>;
+export default function index() {
+  const year = new Date().getFullYear();
+  return (
+    <Wrapper>
+      <p>
+        Made by {""}
+        <span>Durim</span> — Copyright {year}
+      </p>
+      <div className="">
+        <SocialMedia />
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-  padding: 20px 100px;
+  padding: 50px 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid #696969;
+
+  p {
+    font-size: 16px;
+    font-weight: 600;
+    color: #959595;
+
+    span {
+      color: #fff;
+    }
+  }
 `;
