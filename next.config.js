0f1c68db4b42e5953bd51bd2834387e00b00a61d
durimfe <durim.dev@gmail.com>
2024-10-17 15:46:14 +0200

    added turbo

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
