4aee0f4d23f59896e68661a2896230d92804ee54
durimfe <durim.dev@gmail.com>
2024-10-17 15:12:07 +0200

    removed experimental  app router

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
